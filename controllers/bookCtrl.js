5084966cc230776ce77aa94f2fa48548949854da
Mujib <mujib517@gmail.com>
2022-10-28 08:38:13 +0530

    PUT and Patch methods

--- controllers/bookCtrl.js
+++ controllers/bookCtrl.js
@@ -54,9 +54,74 @@ const remove = (req, res) => {
     res.send();
 };
 
+// http://localhost:3000/books/1
+// {price:120,name:'Refactoring 2'}
+// full update
+const put = (req, res) => {
+    const id = +req.params.id;
+    const data = req.body;
+
+    if (!data.name || !data.price) {
+
+        const errors = [];
+
+        if (!data.name) {
+            const err = { msg: 'Name is required' };
+            errors.push(err);
+        }
+
+        if (!data.price) {
+            const err = { msg: 'Price is required' };
+            errors.push(err);
+        }
+
+        res.status(400);
+        res.json(errors);
+    }
+
+    for (let i = 0; i < books.length; i++) {
+        if (books[i].id === id) {
+            books[i].name = data.name;
+            books[i].price = data.price;
+
+            res.status(204);
+            res.send();
+            return;
+        }
+    }
+
+    res.status(404);
+    res.send('Not found');
+};
+
+// partial update
+const patch = (req, res) => {
+    const id = +req.params.id;
+    const data = req.body;
+
+    for (let i = 0; i < books.length; i++) {
+        if (books[i].id === id) {
+            const book = books[i];
+            // reflection
+            for (let key in data) {
+                book[key] = data[key];
+            }
+
+            res.status(204);
+            res.send();
+            return;
+        }
+    }
+
+    res.status(404);
+    res.send('Not found');
+};
+
 module.exports = {
     get,
     post,
     remove,
     getById,
+    put,
+    patch,
 }
--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -7,5 +7,7 @@ router.get('/', bookCtrl.get);
 router.get('/:id', bookCtrl.getById);
 router.post('/', bookCtrl.post);
 router.delete('/:id', bookCtrl.remove);
+router.put('/:id', bookCtrl.put);
+router.patch('/:id', bookCtrl.patch);
 
 module.exports = router;
