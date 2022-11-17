59d5b71c53add24e5e637be3c48982ef7af25f19
Mujib <mujib517@gmail.com>
2022-11-17 09:18:59 +0530

    cluster fork new instance on exit

--- main.js
+++ main.js
@@ -2,11 +2,22 @@ const express = require('express');
 const os = require('os');
 const cluster = require('cluster');
 
+// Javascript
+// NodeJS REST Unifrom interface, auth, authorization, pagination
+// React
+// linkedin Ruby 23 servers
+// NodeJS 3 servers 20%
+// aws, azure
 if (cluster.isMaster) {
     const cores = os.cpus().length;
     for (let i = 0; i < cores; i++) {
         cluster.fork();
     }
+
+    cluster.on('exit', worker => {
+        console.log('worker is shutdown', worker.id);
+        cluster.fork();
+    });
 } else {
     const app = express();
 
